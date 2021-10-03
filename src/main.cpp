355112670cb58f047de0d9858a679d115770d13d
Gaurav Patel <GauravPatel97@outlook.com>
2021-10-03 22:01:37 +0530

    SDL integration complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Chip8.h>
-
+#include <SDL.h>
 #include <iostream>
 #include <bitset>
 
-int main()
+int main(int argc, char *argv[])
 {
     Chip8 *ch8{new Chip8()};
     std::cout << "Current Program Counter location: " << ch8->pc << '\n';
@@ -19,5 +19,26 @@ int main()
             std::cout << '\n';
         }
     }
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
